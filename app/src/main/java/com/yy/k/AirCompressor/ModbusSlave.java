510c0c0e3451c06e1c8893ea2c4c19d3f545e59a
krguang <526110805@qq.com>
2018-02-25 09:23:20 +0800

    完善协议地址

--- app/src/main/java/com/yy/k/AirCompressor/ModbusSlave.java
+++ app/src/main/java/com/yy/k/AirCompressor/ModbusSlave.java
@@ -17,65 +17,22 @@ import static android.content.ContentValues.TAG;
 public class ModbusSlave extends Thread{
 
 
-    private ArrayList<Byte> rxTemp = new ArrayList();
+    private ArrayList<Byte> rxTemp = new ArrayList<Byte>();
     private Serial com2=new Serial();
     private Timer timer10ms=new Timer();
     private boolean rxFlag;
-    private final int SLAV_addr=1;
-    int[] regHodingBuf = new int[1024];
 
-    public boolean allowWriteShiDuSet = true;
-    public boolean allowWriteWenDuSet = true;
+    private int SLAV_addr=1;
+    private int[] localArray = new int[1024];
 
     private int jiZuStartStop = 0;
-    private int zhiBanStartStop = 0;
-    private int fuYaStartStop = 0;
-    private int wenDuSet = 250;
-    private int shiDuSet = 500;
-    private int yaChaSet = 500;
-    private int wenDu = 250;
-    private int shiDu = 500;
-    private short yaCha = 500;
-    private short fengJiZhuangTai = 0;
-    private short zhiBanZhuangTai = 0;
-    private short fuYaZhuangtai = 0;
-    private short fengJiGuZhang = 0;
-    private short GaoXiao;
+    private int temperature = 250;
+    private int humidity = 500;
+    private int pressure = 500;
 
-    private short ColdWaterValveOpening = 0;//冷水阀
-    private short HotWaterValveOpening = 0;//热水阀
-    private short HumidifieOpening = 0;   //加湿器
-    private short TheAirTemperature = 0;//新风温度
+    private int pressureUpperLimit = 900;
+    private int pressureLowerLimit =200;
 
-    private short upperComputerHeartBeatMonitoringPoint = 0;       //上位机心跳监控点
-    private short upperComputerHandAutomaticallyMonitoringPoint = 0;//上位机手自动监控点
-    private short upperComputerFengjiZHuangTaiMonitoringPoint;//上位机风机状态监控点
-    private short upperComputerZhongXiaoMonitoringPoint;//上位机盘管低温监控点
-    private short upperComputerGaoXiaoMonitoringPoint;//上位机高效报警监控点
-    private short upperComputerChuXiaoMonitoringPoint;//上位机中效报警监控点
-    private short upperComputerElectricWarmOneMonitoringPoint;//上位机电加热1监控点
-    private short upperComputerElectricWarmTwoMonitoringPoint;//上位机电加热2监控点
-    private short upperComputerElectricWarmThreeMonitoringPoint;//上位机电加热3监控点
-    private short upperComputerElectricWarmHighTemperatureMonitoringPoint;//上位机电加热高温监控点
-    private short upperComputerFengJiQueFengMonitoringPoint;//上位机风机缺风监控点
-    private short upperComputerSterilizationMonitoringPoint;//上位机灭菌监控点
-    private short upperComputerFengJiStartMonitoringPoint;//上位机风机已启动监控点
-    private short upperComputerPaiFengJiStartMonitoringPoint;//上位机排风机已启动监控点
-    private short upperComputerZhiBanStartMonitoringPoint;//上位机值班已启动监控点
-    private short upperComputerFuYaStartMonitoringPoint;//上位机负压启动监控点
-    private short upperComputerElectricPreheatOneMonitoringPoint;//上位机电预热1监控点
-    private short upperComputerElectricPreheatTwoMonitoringPoint;//上位机电预热2监控点
-    private short upperComputerElectricPreheatThreeMonitoringPoint;//上位机电预热3监控点
-    private short upperComputerElectricPreheatHighTemperatureMonitoringPoint;//上位机电预热高温监控点
-    private short upperComputerCompressorOneStartMonitoringPoint;//上位机压缩机1运行监控点
-    private short upperComputerCompressorTwoStartMonitoringPoint;//上位机压缩机2运行监控点
-    private short upperComputerCompressorThreeStartMonitoringPoint;//上位机压缩机3运行监控点
-    private short upperComputerCompressorFourStartMonitoringPoint;//上位机压缩机4运行监控点
-    private short upperComputerCompressorOneBreakdownMonitoringPoint;//上位机压缩机1故障监控点
-    private short upperComputerCompressorTwoBreakdownMonitoringPoint;//上位机压缩机2故障监控点
-    private short upperComputerCompressorThreeBreakdownMonitoringPoint;//上位机压缩机3故障监控点
-    private short upperComputerCompressorFourBreakdownMonitoringPoint;//上位机压缩机4故障监控点
-    private short WinterInSummer = 0;//冬夏季
 
     @Override
     /**
@@ -190,8 +147,8 @@ public class ModbusSlave extends Thread{
             seBuf[2] = (byte) (2 * len);
 
             for (int i = 0; i < len; i++) {
-                seBuf[3 + 2 * i] = (byte) (crc.getUnsignedIntt(regHodingBuf[i + addr]) >> 8);
-                seBuf[4 + 2 * i] = (byte) (crc.getUnsignedIntt(regHodingBuf[i + addr]));
+                seBuf[3 + 2 * i] = (byte) (crc.getUnsignedIntt(localArray[i + addr]) >> 8);
+                seBuf[4 + 2 * i] = (byte) (crc.getUnsignedIntt(localArray[i + addr]));
 
             }
 
@@ -211,8 +168,6 @@ public class ModbusSlave extends Thread{
             disTemp[i] = seBuf[i];
         }
         Log.d(TAG, "mod_Fun_03_Slav: "+ Arrays.toString(disTemp));
-
-
     }
 
 
@@ -235,13 +190,12 @@ public class ModbusSlave extends Thread{
      */
 
     private void slav_int_03() {
-        regHodingBuf[0] = jiZuStartStop;
-        regHodingBuf[1] = zhiBanStartStop;
-        regHodingBuf[2] = 0;//预留
-        regHodingBuf[3] = fuYaStartStop;
-        regHodingBuf[4] = 0;//预留
-        regHodingBuf[5] = wenDuSet;
-        regHodingBuf[6] = shiDuSet;
+        if (jiZuStartStop == 0){
+            localArray[0] &= ~0x01;
+        }else {
+            localArray[0] |= 0x01;
+        }
+
     }
 
     /***
@@ -268,7 +222,7 @@ public class ModbusSlave extends Thread{
             /***
              * 取起始地址开始的数据
              */
-            regHodingBuf[addr + i] = val;
+            localArray[addr + i] = val;
         }
 
         for (int i = 0; i < 6; i++) {
@@ -289,60 +243,38 @@ public class ModbusSlave extends Thread{
      */
     private void slav_hand_10() {
 
-        if (allowWriteWenDuSet) {
-            wenDuSet = (short) regHodingBuf[5];
-        }
-        if (allowWriteShiDuSet) {
-            shiDuSet = (short) regHodingBuf[6];
-        }
-
-        wenDu = (short) regHodingBuf[7];
-        shiDu = (short) regHodingBuf[8];
-        fengJiZhuangTai = (short) regHodingBuf[9];
-        //zhiBanZhuangTai = (short) regHodingBuf[10];
-        zhiBanZhuangTai=(short)(regHodingBuf[10]&0x01);
-        //upperComputerFuYaStartMonitoringPoint = (short) ((regHodingBuf[10] & 0x02) >> 1);                      //上位机负压启动监控点
-        fuYaZhuangtai = (short) ((regHodingBuf[10] & 0x02) >> 1);                      //上位机负压启动监控点
-
-        fengJiGuZhang = (short) regHodingBuf[11];
-        GaoXiao = (short) regHodingBuf[12];
-        ColdWaterValveOpening = (short) regHodingBuf[13];//冷水阀开度
-        HotWaterValveOpening = (short) regHodingBuf[14];//热水阀开度
-        HumidifieOpening = (short) regHodingBuf[15];  //加湿器开度
-        TheAirTemperature = (short) regHodingBuf[16]; //新风温度
-
-        upperComputerHeartBeatMonitoringPoint = (short) ((regHodingBuf[17] & 0x0100) >> 8);                    //上位机心跳监控点
-        upperComputerHandAutomaticallyMonitoringPoint = (short) ((regHodingBuf[17] & 0x0200) >> 9);            //上位机手自动监控点
-        upperComputerFengjiZHuangTaiMonitoringPoint = (short) ((regHodingBuf[17] & 0x0400) >> 10);             //上位机风机状态监控点
-        upperComputerZhongXiaoMonitoringPoint = (short) ((regHodingBuf[17] & 0x0800) >> 11);                     //上位机中效报警监控点
-        upperComputerGaoXiaoMonitoringPoint = (short) ((regHodingBuf[17] & 0x1000) >> 12);                     //上位机高效报警监控点
-        upperComputerChuXiaoMonitoringPoint = (short) ((regHodingBuf[17] & 0x2000) >> 13);                     //上位机初效报警监控点
-        upperComputerElectricWarmOneMonitoringPoint = (short) ((regHodingBuf[17] & 0x4000) >> 14);             //上位机电加热1监控点
-        upperComputerElectricWarmTwoMonitoringPoint = (short) ((regHodingBuf[17] & 0x8000) >> 15);             //上位机电加热2监控点
-
-        upperComputerElectricWarmThreeMonitoringPoint = (short) (regHodingBuf[17] & 0x01);                     //上位机电加热3监控点
-        upperComputerElectricWarmHighTemperatureMonitoringPoint = (short) ((regHodingBuf[17] & 0x02) >> 1);    //上位机电加热高温监控点
-        upperComputerFengJiQueFengMonitoringPoint = (short) ((regHodingBuf[17] & 0x04) >> 2);                  //上位机风机缺风监控点
-        upperComputerSterilizationMonitoringPoint = (short) ((regHodingBuf[17] & 0x08) >> 3);                  //上位机灭菌监控点
-        upperComputerFengJiStartMonitoringPoint = (short) ((regHodingBuf[17] & 0x10) >> 4);                    //上位机风机已启动监控点
-        upperComputerPaiFengJiStartMonitoringPoint = (short) ((regHodingBuf[17] & 0x20) >> 5);                 //上位机排风机已启动监控点
-        upperComputerZhiBanStartMonitoringPoint = (short) ((regHodingBuf[17] & 0x40) >> 6);                    //上位机值班已启动监控点
-        //   upperComputerFuYaStartMonitoringPoint = (short) ((regHodingBuf[17] & 0x80) >> 7);                      //上位机负压启动监控点
-/*
-        upperComputerElectricPreheatOneMonitoringPoint = (short) ((regHodingBuf[18] & 0x10) >> 4);             //上位机电预热1监控点
-        upperComputerElectricPreheatTwoMonitoringPoint = (short) ((regHodingBuf[18] & 0x20) >> 5);             //上位机电预热2监控点
-        upperComputerElectricPreheatThreeMonitoringPoint = (short) ((regHodingBuf[18] & 0x40) >> 6);           //上位机电预热3监控点
-        upperComputerElectricPreheatHighTemperatureMonitoringPoint = (short) ((regHodingBuf[18] & 0x80) >> 7); //上位机电预热高温监控点
-        upperComputerCompressorOneStartMonitoringPoint = (short) ((regHodingBuf[18] & 0x0100) >> 8);           //上位机压缩机1运行监控点
-        upperComputerCompressorTwoStartMonitoringPoint = (short) ((regHodingBuf[18] & 0x0200) >> 9);           //上位机压缩机2运行监控点
-        upperComputerCompressorThreeStartMonitoringPoint = (short) ((regHodingBuf[18] & 0x0400) >> 10);        //上位机压缩机3运行监控点
-        upperComputerCompressorFourStartMonitoringPoint = (short) ((regHodingBuf[18] & 0x0800) >> 11);         //上位机压缩机4运行监控点
-        upperComputerCompressorOneBreakdownMonitoringPoint = (short) (regHodingBuf[18] & 0x01);                //上位机压缩机1故障监控点
-        upperComputerCompressorTwoBreakdownMonitoringPoint = (short) ((regHodingBuf[18] & 0x02) >> 1);         //上位机压缩机2故障监控点
-        upperComputerCompressorThreeBreakdownMonitoringPoint = (short) ((regHodingBuf[18] & 0x04) >> 2);       //上位机压缩机3故障监控点
-        upperComputerCompressorFourBreakdownMonitoringPoint = (short) ((regHodingBuf[18] & 0x08) >> 3);        //上位机压缩机4故障监控点
-        WinterInSummer = (short) ((regHodingBuf[20] & 0x04) >> 2);                                             //冬夏季监控控制点偏移2
-    */
+        localArray[6] = temperature;
+        localArray[7] = humidity;
+        localArray[8] = pressure;
+        localArray[9] = pressureUpperLimit;
+        localArray[10] = pressureLowerLimit;
     }
 
+    public void setSLAV_addr(int SLAV_addr) {
+        this.SLAV_addr = SLAV_addr;
+    }
+
+    public void setJiZuStartStop(int jiZuStartStop) {
+        this.jiZuStartStop = jiZuStartStop;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public void setPressure(int pressure) {
+        this.pressure = pressure;
+    }
+
+    public void setPressureUpperLimit(int pressureUpperLimit) {
+        this.pressureUpperLimit = pressureUpperLimit;
+    }
+
+    public void setPressureLowerLimit(int pressureLowerLimit) {
+        this.pressureLowerLimit = pressureLowerLimit;
+    }
 }
